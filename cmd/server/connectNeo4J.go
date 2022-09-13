51c015c12afbf41558c4a149e5b97efaff7e5f94
rguarnizo <rguarnizo@unal.edu.co>
2022-09-12 21:28:14 -0500

    Changing DB used

--- cmd/server/connectNeo4J.go
+++ cmd/server/connectNeo4J.go
@@ -7,12 +7,12 @@ import (
 )
 
 func neo4jConnection() neo4j.Session {
-	driver, err := neo4j.NewDriver("bolt://localhost:7687", neo4j.BasicAuth("neo4j", `test`, ""))
+	driver, err := neo4j.NewDriver("bolt://localhost:7687", neo4j.BasicAuth("neo4j", "test", ""))
 
 	if err != nil {
 		log.Fatal("Error Connecting to Neo4J DB")
 	}
 
-	session := driver.NewSession(neo4j.SessionConfig{AccessMode: neo4j.AccessModeWrite, DatabaseName: "inscripciones"})
+	session := driver.NewSession(neo4j.SessionConfig{AccessMode: neo4j.AccessModeWrite})
 	return session
 }
